e9afb7bf19cca5035da5a0bc1c74812d7af28ade
Poe Mei <poemei761@gmail.com>
2026-03-12 19:36:49 -0700

    conditions

--- app/controllers/resume.php
+++ app/controllers/resume.php
@@ -10,6 +10,18 @@ final class resume extends controller
             'get_resume' => $resume_model->get_resume()
         ];
         
-        $this->view('public/resume/index', $data);
+        /**
+         * Add conditions check
+         * if not found -> 404 to the error_handler
+         * [Human:Mei | 2026-03-13 02:35:00 UTC]
+        */
+        $view = 'public/resume/index';
+
+        if (!file_exists(APPROOT . '/views/' . $view . '.php')) {
+            (new error_handler())->not_found();
+            return;
+        }
+
+        $this->view($view, $data);
     }
 }
